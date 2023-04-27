966f5f356e037bd23fa6f3bc3157333a2172eb37
David Quichocho <dave@ucr-secure-44-10-13-227-75.wnet.ucr.edu>
2023-04-26 18:17:07 -0700

    Model and Controller for Inventory added

---
+++ CS180ChefsAnonymous/Models/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CS180ChefsAnonymous.Models
+{
+	public class Inventory
+	{
+		public int InventoryId { get; set; }
+		public int UserId { get; set; }
+		public string? ItemName { get; set; } = "";
+		public int Qty { get; set; }
+		public int unit { get; set; }
+		public Inventory()
+		{
+		}
+	}
+}
+
