261b793d575b224508b67d5a081a46df40048820
David Quichocho <dave@ucr-secure-44-10-13-227-75.wnet.ucr.edu>
2023-04-26 17:58:28 -0700

    Models for MealPlan and Items added, removed the duplicate models for Recipes and Ingredients

---
+++ CS180ChefsAnonymous/Models/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS180ChefsAnonymous.Models
+{
+	public class Items
+	{
+		public string? ItemName { get; set; } = "";
+		public int CalPerKg { get; set; }
+		public string? OtherInfo { get; set; } = "";
+
+		public Items()
+		{
+		}
+	}
+}
+
---
+++ CS180ChefsAnonymous/Models/MealPlan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CS180ChefsAnonymous.Models
+{
+	public class MealPlan
+	{
+		public int MealPlanId { get; set; }
+		public DateTime MealTime { get; set; }
+		public DayOfWeek DayOfTheWeek { get; set; }
+		public int Recipe_id { get; set; }
+
+		public MealPlan()
+		{
+		}
+	}
+}
+
