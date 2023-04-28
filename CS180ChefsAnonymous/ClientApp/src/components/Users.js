93045bcca4e9f06845656d36a9bf90d5b4ac5ef5
Jay <upadhyjay@gmail.com>
2023-04-28 15:09:39 -0700

    Able to add recipe to db but not showing visually yet

--- CS180ChefsAnonymous/ClientApp/src/components/Users.js
+++ CS180ChefsAnonymous/ClientApp/src/components/Users.js
@@ -74,12 +74,99 @@ export function Users() {
             .then(data => console.log(data))
             .catch(error => console.error(error));
     }
+// Recipe stuff
+    const [recipes, setRecipes] = useState([]);
+    useEffect(() => {
+        fetch("api/recipe/GetRecipes")
+            .then((response) => response.json())
+            //.then((response) => {
+            //    //console.log(response.json());
+            //    response.json()
+            //})
+            .then((responseJson) => {
+                console.log(responseJson);
+                setRecipes(responseJson);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    function refreshRecipe() {
+        fetch("api/recipe/GetRecipes")
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                setRecipes(responseJson);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    function addRecipe() {
+        fetch("api/recipe/AddRecipe", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                // pass the new user object as the request body
+                recipe_id: 6,
+                reciple_title: 'Cookie',
+                recipe_desc: 'Cookie Description',
+                instructions: 'Do stuff',
+                prep_time: 5,
+                cooking_time: 5,
+                user_id: 6,
+                category_id: 1
+
+            })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    }
+
+    function updateRecipe() {
+        fetch("api/recipe/UpdateRecipe/1", {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                // pass the new user object as the request body
+                recipe_id: 6,
+                reciple_title: 'Cookie2',
+                recipe_desc: 'Cookie2 Description',
+                instructions: 'Do stuff2',
+                prep_time: 5,
+                cooking_time: 5,
+                user_id: 6,
+                category_id: 1
+
+            })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    }
+    function removeRecipe() {
+        fetch("api/recipe/DeleteRecipe/6", {
+            method: 'DELETE',
+
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    }
+
 
     //};
 
     return (
         <div className="container">
-            <h1>Users</h1>
+            <h1>Items</h1>
             <div className="row">
                 <div className="col-sm-12">
                     <table className="table table-striped">
@@ -122,8 +209,55 @@ export function Users() {
                 Delete
             </button >
 
-
+            <h1>Recipes</h1>
+            <div className="row">
+                <div className="col-sm-12">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>RecipeID</th>
+                                <th>RecipeTitle</th>
+                                <th>RecipeDescription</th>
+                                <th>Instructions</th>
+                                <th>Preptime</th>
+                                <th>CookingTime</th>
+                                <th>RecipeDescription</th>
+                                <th>UserID</th>
+                                <th>CategoryID</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map((recipe) => (
+                                    <tr key={recipe.recipe_title}>
+                                        <td>{recipe.recipe_id}</td>
+                                        <td>{recipe.recipe_title}</td>
+                                        <td>{recipe.recipe_desc}</td>
+                                        <td>{recipe.instructions}</td>
+                                        <td>{recipe.prep_time}</td>
+                                        <td>{recipe.cooking_time}</td>
+                                        <td>{recipe.user_id}</td>
+                                        <td>{recipe.category_id}</td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <button className="btn btn-primary" onClick={refreshRecipe}>
+                Refresh
+            </button >
+            <button className="btn btn-primary" onClick={addRecipe}>
+                Add
+            </button >
+            <button className="btn btn-primary" onClick={updateRecipe}>
+                Update
+            </button >
+            <button className="btn btn-primary" onClick={removeRecipe}>
+                Delete
+            </button >
         </div>
+        
     );
 }
 
