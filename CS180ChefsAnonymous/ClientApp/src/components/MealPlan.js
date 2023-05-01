6dc4b1a6922166fa22acf6872a7394777ec316ac
Haruki Miyazaki <baskekami19990818@gmail.com>
2023-04-30 22:07:31 -0700

    created base table for meal plan

--- CS180ChefsAnonymous/ClientApp/src/components/MealPlan.js
+++ CS180ChefsAnonymous/ClientApp/src/components/MealPlan.js
@@ -1,39 +1,74 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useMemo, useEffect } from "react";
 import data from "../dummy-meal-plan.json";
 
 const MealPlan = (props) => {
-  const [plans, setPlans] = useState(data);
+  const rows = 5;
+  const cols = 8;
+
+  function getDay(j){
+    if (j===0) return 'Time'
+    else if (j===1) return 'Sun'
+    else if (j===2) return 'Mon'
+    else if (j===3) return 'Tue'
+    else if (j===4) return 'Wed'
+    else if (j===5) return 'Thu'
+    else if (j===6) return 'Fri'
+    else if (j===7) return 'Sat'
+  }
+
+  function getMealTime(i) {
+    if (i===1) return 'Breakfast'
+    else if (i===2) return 'Lunch'
+    else if (i===3) return 'Snacks'
+    else if (i===4) return 'Dinner'
+    else if (i===5) return 'Dessert'
+  }
+
+  // useEffect(() => {
+  //   // Runs after EVERY rendering
+  // });
+
+  const tableRows = [];
+  for (let i = 0; i < rows; i++) {
+    const tableCells = [];
+    for (let j = 0; j < cols; j++) {
+      if (i===0){
+        tableCells.push(
+          <td id={i+'-'+j} key={i+'-'+j}>
+            {getDay(j)}
+          </td>
+        );
+      }
+      else {
+        if (j === 0 && i !== 0) {
+          tableCells.push(
+            <td id={i+'-'+j} key={i+'-'+j}>
+              {getMealTime(i)}
+            </td>
+          );
+        }
+        else {
+          tableCells.push(
+            <td id={i+'-'+j} key={i+'-'+j}>
+              ({i},{j})
+            </td>
+          );
+        }
+      }
+    }
+    tableRows.push(
+      <tr id={i} key={i}>
+        {tableCells}
+      </tr>
+    );
+  }
+
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Meal Time</th>
-            <th>Sun</th>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>Sat</th>
-          </tr>
-        </thead>
-        <tbody>
-          {plans.map( (plan) => (
-            <tr>
-              <td>{plan.mealTime}</td>
-              <td>{plan.Sun}</td>
-              <td>{plan.Mon}</td>
-              <td>{plan.Tue}</td>
-              <td>{plan.Wed}</td>
-              <td>{plan.Thu}</td>
-              <td>{plan.Fri}</td>
-              <td>{plan.Sat}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <tbody>
+        {tableRows}
+      </tbody>
+    </table>
   );
 };
 
