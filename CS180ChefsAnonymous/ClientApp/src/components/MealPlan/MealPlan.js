03d2779affd68f5ccc952566f38b28e312e87f39
Haruki Miyazaki <baskekami19990818@gmail.com>
2023-05-03 00:22:38 -0700

    display recipe_id in a cell from a user_id

--- CS180ChefsAnonymous/ClientApp/src/components/MealPlan/MealPlan.js
+++ CS180ChefsAnonymous/ClientApp/src/components/MealPlan/MealPlan.js
@@ -3,24 +3,21 @@ import styles from "./MealPlan.css";
 import data from "../../dummy-meal-plan.json";
 
 const MealPlan = (props) => {
-  const [modal, setModal] = useState(false);
+  const [isModal, setModal] = useState(false);
+  // recipe is a recipe we can retrieve by clicking a cell
   const [recipe, setRecipe] = useState("");
   const [meals, setMeals] = useState([])
+  // meal_matrix is a table which contains recipe_id for displaying in a cell of table
   const [meal_matrix, setMealMatrix] = useState(Array.from({length: 5},()=> Array.from({length: 7}, () => null)));
-  console.log(meal_matrix)
 
-  // const toggleModal = () => {
-  //   setModal(!modal);
-  // }
 
   const toggleModal = (i,j) => {
-    setModal(!modal);
+    setModal(!isModal);
     setRecipe(filteredData.filter((jsonData) => jsonData.mealTime === i && jsonData.day_of_week === j)[0]?.recipe_id)
   }
 
-  if (modal) {
+  if (isModal) {
     document.body.classList.add('active-modal');
-    // document.getElementById("modal").style.display = "block";
   } else {
     document.body.classList.remove('active-modal');
   }
@@ -47,20 +44,18 @@ const MealPlan = (props) => {
     else if (i===5) return 'Dessert'
   }
 
-  // useEffect(() => {
-  //   // Runs after EVERY rendering
-  // });
+  useEffect(() => {
+    // Runs after EVERY rendering
+  });
 
   const filteredData = data.filter( (jsonData) => jsonData.user_id === 1);
-  // console.log("filtered data:",filteredData);
 
-  const handleChange = (row, col, event) => {
-    let copy = [...meal_matrix];
-    copy[row][col] = filteredData.filter((jsonData) => jsonData.mealTime === row + 1 && jsonData.day_of_week === col + 1)[0]?.recipe_id;
-    setMealMatrix(copy);
-
-    console.log("meal matrix:",meal_matrix);
-  };
+  let copy = [...meal_matrix];
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < cols; j++) {
+      copy[i-1][j-1] = filteredData.filter((jsonData) => jsonData.mealTime === i && jsonData.day_of_week === j)[0]?.recipe_id;
+    }
+  }
 
   const tableRows = [];
   for (let i = 0; i < rows; i++) {
@@ -84,12 +79,7 @@ const MealPlan = (props) => {
         else {
           tableCells.push(
             <td id={i+'-'+j} key={i+'-'+j} onClick={ () => toggleModal(i,j)}>
-              ({i},{j})
               {meal_matrix[i-1][j-1]}
-              <input
-                type="text"
-                onChange={e => handleChange(i-1, j-1, e)}
-              />
             </td>
           );
         }
@@ -109,7 +99,7 @@ const MealPlan = (props) => {
           {tableRows}
         </tbody>
       </table>
-      {modal && (
+      {isModal && (
         <div className="modal1">
           <div onClick={toggleModal} className="overlay1"></div>
           <div className="modal-content1">
