210d78095ad1c8270987d5c98a2abce30fc884fd
mgnChn <86279978+mgnChn@users.noreply.github.com>
2023-05-08 00:34:50 -0700

    Update DropdownMenu.js

---
+++ CS180ChefsAnonymous/ClientApp/src/components/UI/DropdownMenu.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "../UI/DropdownMenu.module.css";
+
+const Icon = () => {
+  return (
+    <svg height="20" width="20" viewBox="0 0 20 20">
+      <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
+    </svg>
+  );
+};
+
+const DropdownMenu = (props) => {
+  const getDisplay = () => {
+    return props.placeHolder;
+  };
+
+  return (
+    <div className={styles.dropdown_container}>
+      <div className={styles.dropdown_input}>
+        <div className={styles.dropdown_menu}>
+          {props.options.map((option) => (
+            <div key={option.id} className={styles.dropdown_item}>
+              {option.label}
+            </div>
+          ))}
+        </div>
+        {/* <div className={styles.dropdown_selected_value}>{getDisplay()}</div>
+        <div className={styles.dropdown_tools}>
+          <div className={styles.dropdown_tool}>
+            <Icon />
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default DropdownMenu;
