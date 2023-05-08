5e13f42d069e52f0fbbbd8f0e45ddffdb2632592
Haruki Miyazaki <baskekami19990818@gmail.com>
2023-05-07 22:25:56 -0700

    considered the case when there's no meal plan yet

--- CS180ChefsAnonymous/ClientApp/src/components/MealPlan/MealPlanModal.js
+++ CS180ChefsAnonymous/ClientApp/src/components/MealPlan/MealPlanModal.js
@@ -3,11 +3,16 @@ import RecipesList from '../Recipes/RecipesList'
 import DUMMY_RECIPES from "../Recipes/dummy-recipe-data.json";
 
 const MealPlanModal = ({toggleModal, recipe}) => {
+  console.log(recipe)
+  var recipe_name = ""
+  if (recipe === undefined) { recipe_name = "-" }
+  else { recipe_name = recipe }
   return (
     <div className="modal1">
         <div onClick={toggleModal} className="overlay1"/>
         <div className="modal-content1">
-            <h3 className="mealplan-modal-head">Recipe Name: {recipe}</h3>
+            <h3 className="mealplan-modal-head">Recipe Name: {recipe_name}</h3>
+            <p>You can select a meal:</p>
             <div className="meal-modal-list">
                 <RecipesList recipes={DUMMY_RECIPES}/>
             </div>
