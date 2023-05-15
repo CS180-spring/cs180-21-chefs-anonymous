5fcd6a44e33b8ef7df51291cb9e4fa56d6ce1621
Advika Suri <asuri010@ucr.edu>
2023-05-15 10:44:47 -0700

    added the ability to display and cancel the form when click

--- CS180ChefsAnonymous/ClientApp/src/components/Inventory/InventoryItem.js
+++ CS180ChefsAnonymous/ClientApp/src/components/Inventory/InventoryItem.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useContext, useState} from "react";
+import InventoryContext from "./InventoryContext"
 import Card from "../UI/Card";
 import styles from "./InventoryItem.module.css";
-
+import Button from "../UI/Button";
+import Modal from "../UI/Modal";
+import styles from "./InventoryItem.module.css";
+import InventoryForm from "./InventoryForm";
 
 
 
 const InventoryItem = (props) => {
+  const context = useContext(InventoryContext);
+  const [displayForm, setDisplayForm] = useState(false);
+  const [displayDeleteModal, setDisplayDeleteModal] = useState(false);
+  const [displayEditDelete, setDisplayEditDelete] = useState(false);
+
+
+
+
   const mouseOverHandler = (event) => {
     event.target.style.background = "#dfe6e3";
   };
