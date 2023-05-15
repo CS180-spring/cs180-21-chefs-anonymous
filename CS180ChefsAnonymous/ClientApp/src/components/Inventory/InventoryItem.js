e416018885dc2ba3a4ebbd646baa06163e159025
Advika Suri <asuri010@ucr.edu>
2023-05-15 10:33:16 -0700

    adding delete and edit properties

--- CS180ChefsAnonymous/ClientApp/src/components/Inventory/InventoryItem.js
+++ CS180ChefsAnonymous/ClientApp/src/components/Inventory/InventoryItem.js
@@ -2,7 +2,12 @@ import React from "react";
 import Card from "../UI/Card";
 import styles from "./InventoryItem.module.css";
 
+
+
+
 const InventoryItem = (props) => {
+  const [displayDeleteModal, setDisplayDeleteModal] = useState(false);
+  const [displayEditDelete, setDisplayEditDelete] = useState(false);
   const mouseOverHandler = (event) => {
     event.target.style.background = "#dfe6e3";
   };
